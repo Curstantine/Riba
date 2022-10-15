31182223166549a3e11319cd25dada83f9749bf2
Curstantine <resistlyfe@gmail.com>
2022-10-15 22:48:43 +0530

    chore: Moved from string literals to resource strings.

--- app/src/main/java/moe/curstantine/riba/ui/common/components/FlexibleErrorReceiver.kt
+++ app/src/main/java/moe/curstantine/riba/ui/common/components/FlexibleErrorReceiver.kt
@@ -2,12 +2,10 @@ package moe.curstantine.riba.ui.common.components
 
 import android.content.Intent
 import android.net.Uri
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -21,47 +19,59 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import moe.curstantine.riba.R
 import moe.curstantine.riba.api.mangadex.DexConstants
 import moe.curstantine.riba.api.mangadex.DexError
+import moe.curstantine.riba.api.riba.RibaConstants
 import moe.curstantine.riba.api.riba.RibaError
 
 @Composable
 fun FlexibleErrorReceiver(error: RibaError) {
     val colorScheme = MaterialTheme.colorScheme
     val typography = MaterialTheme.typography
+    val localCtx = LocalContext.current
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .horizontalScroll(rememberScrollState())
             .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = error.human, style = typography.bodyLarge)
+        Text(
+            text = error.human,
+            style = typography.bodyLarge.copy(fontWeight = FontWeight.SemiBold)
+        )
 
         if (error.additional != null) {
             Text(
                 text = error.additional!!,
                 style = typography.bodySmall.copy(color = colorScheme.onSurface.copy(alpha = 0.75F)),
+                textAlign = TextAlign.Center
             )
         }
 
         Box(Modifier.height(16.dp))
         Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-
             if (error is DexError) {
-                val localCtx = LocalContext.current
-                val intent =
-                    remember { Intent(Intent.ACTION_VIEW, Uri.parse(DexConstants.STATUS_PAGE)) }
-
-                FilledTonalButton(onClick = { localCtx.startActivity(intent) }) {
-                    Text("Status Page")
+                val dexStatusIntent = remember {
+                    Intent(Intent.ACTION_VIEW, Uri.parse(DexConstants.STATUS_PAGE))
+                }
+                FilledTonalButton(onClick = { localCtx.startActivity(dexStatusIntent) }) {
+                    Text(stringResource(R.string.status_page))
                 }
             }
 
+            val issueIntent = remember {
+                Intent(Intent.ACTION_VIEW, Uri.parse(RibaConstants.ISSUES_URL))
+            }
+            FilledTonalButton(onClick = { localCtx.startActivity(issueIntent) }) {
+                Text(stringResource(R.string.create_issue))
+            }
         }
     }
 }
