19cb2597339a2553df79144d03df49e5853c3331
Rachala Ovin <resistlyfe@gmail.com>
2022-10-31 14:36:40 +0530

    chore: updated dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,105 +1,107 @@
-object AppConfig {
-    const val applicationId = "moe.curstantine.riba"
-    const val versionCode = 1
-    const val versionName = "1.0"
+@file:Suppress("UnstableApiUsage")
 
-    const val compileSdk = 33
-    const val minSdk = 26
-    const val targetSdk = 33
+object AppConfig {
+	const val applicationId = "moe.curstantine.riba"
+	const val versionCode = 1
+	const val versionName = "1.0"
+
+	const val compileSdk = 33
+	const val minSdk = 26
+	const val targetSdk = 33
 }
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp") version "1.7.10-1.0.6"
+	id("com.android.application")
+	id("org.jetbrains.kotlin.android")
+	id("com.google.devtools.ksp") version "1.7.20-1.0.7"
 }
 
 android {
-    compileSdk = AppConfig.compileSdk
+	compileSdk = AppConfig.compileSdk
 
-    defaultConfig {
-        applicationId = AppConfig.applicationId
-        versionCode = AppConfig.versionCode
-        versionName = AppConfig.versionName
+	defaultConfig {
+		applicationId = AppConfig.applicationId
+		versionCode = AppConfig.versionCode
+		versionName = AppConfig.versionName
 
-        minSdk = AppConfig.minSdk
-        targetSdk = AppConfig.targetSdk
+		minSdk = AppConfig.minSdk
+		targetSdk = AppConfig.targetSdk
 
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-    }
+		vectorDrawables {
+			useSupportLibrary = true
+		}
+	}
 
-    buildTypes {
-        getByName("debug") {
-            isDebuggable = true
-            isMinifyEnabled = false
-            isShrinkResources = false
-            applicationIdSuffix = ".debug"
-            versionNameSuffix = "-debug"
-        }
-        getByName("release") {
-            isDebuggable = false
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles("proguard-android-optimize.txt", "proguard-rules.pro")
-            signingConfig  = signingConfigs.getByName("debug")
-        }
-    }
+	buildTypes {
+		getByName("debug") {
+			isDebuggable = true
+			isMinifyEnabled = false
+			isShrinkResources = false
+			applicationIdSuffix = ".debug"
+			versionNameSuffix = "-debug"
+		}
+		getByName("release") {
+			isDebuggable = false
+			isMinifyEnabled = true
+			isShrinkResources = true
+			proguardFiles("proguard-android-optimize.txt", "proguard-rules.pro")
+			signingConfig = signingConfigs.getByName("debug")
+		}
+	}
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_16
-        targetCompatibility = JavaVersion.VERSION_16
-    }
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_16
+		targetCompatibility = JavaVersion.VERSION_16
+	}
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_16.toString()
-    }
+	kotlinOptions {
+		jvmTarget = JavaVersion.VERSION_16.toString()
+	}
 
-    buildFeatures {
-        compose = true
-    }
+	buildFeatures {
+		compose = true
+	}
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = compose.versions.compiler.version.get()
-    }
+	composeOptions {
+		kotlinCompilerExtensionVersion = compose.versions.compiler.version.get()
+	}
 
-    packagingOptions {
-        resources {
-            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+	packagingOptions {
+		resources {
+			excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+			excludes += "/META-INF/{AL2.0,LGPL2.1}"
+		}
+	}
 
-    namespace = "moe.curstantine.riba"
+	namespace = "moe.curstantine.riba"
 }
 
 dependencies {
-    implementation(androidx.bundles.androidx)
+	implementation(androidx.bundles.androidx)
 
-    implementation(compose.bundles.accompanist)
-    implementation(compose.bundles.activity)
-    implementation(compose.bundles.livedata)
-    implementation(compose.bundles.material)
-    implementation(compose.bundles.navigation)
-    implementation(compose.bundles.ui)
-    implementation(compose.bundles.room)
+	implementation(compose.bundles.accompanist)
+	implementation(compose.bundles.activity)
+	implementation(compose.bundles.livedata)
+	implementation(compose.bundles.material)
+	implementation(compose.bundles.navigation)
+	implementation(compose.bundles.ui)
+	implementation(compose.bundles.room)
 
-    implementation(external.coil)
-    implementation(external.markdown)
-    implementation(external.bundles.moshi)
-    implementation(external.bundles.retrofit)
+	implementation(external.coil)
+	implementation(external.markdown)
+	implementation(external.bundles.moshi)
+	implementation(external.bundles.retrofit)
 
-    ksp(compose.bundles.ksps)
-    ksp(external.bundles.ksps)
+	ksp(compose.bundles.ksps)
+	ksp(external.bundles.ksps)
 }
 
 tasks {
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.freeCompilerArgs += listOf(
-            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
-            "-opt-in=androidx.compose.ui.ExperimentalComposeUiApi",
-            "-opt-in=kotlin.ExperimentalStdlibApi",
-        )
-    }
+	withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+		kotlinOptions.freeCompilerArgs += listOf(
+			"-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+			"-opt-in=androidx.compose.ui.ExperimentalComposeUiApi",
+			"-opt-in=kotlin.ExperimentalStdlibApi",
+		)
+	}
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,17 @@
-buildscript {
-    dependencies {
-        classpath(kotlinx.serialization.gradle)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-    }
-}
+//buildscript {
+//	dependencies {
+//		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+//	}
+//}
 
 // Related: https://youtrack.jetbrains.com/issue/KTIJ-19369
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(androidx.plugins.application) apply false
-    alias(androidx.plugins.library) apply false
-    alias(kotlinx.plugins.android) apply false
+	alias(androidx.plugins.application) apply false
+	alias(androidx.plugins.library) apply false
+	alias(kotlinx.plugins.android) apply false
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+	delete(rootProject.buildDir)
 }
