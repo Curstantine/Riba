94f7f1f7dbf0aa65a093e909f0815b5b22b15928
resistlyfe@gmail.com <resistlyfe@gmail.com>
2023-01-21 15:08:11 +0530

    chore: removed redundant imports

--- lib/routes/home/home_content.dart
+++ lib/routes/home/home_content.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:riba/utils/constants.dart';
-import 'package:riba/utils/theme.dart' show TextColor;
 import 'package:riba/widgets/horizontal_manga_list.dart';
-import 'package:riba/widgets/manga_card.dart';
 
 class HomeContent extends StatelessWidget {
   const HomeContent({super.key});
--- lib/utils/settings.dart
+++ lib/utils/settings.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:riba/utils/theme.dart';
