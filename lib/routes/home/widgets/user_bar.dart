288075b2ae32b738de50ec70183a38d22535d35d
Rachala Ovin <resistlyfe@gmail.com>
2023-05-16 20:18:55 +0530

    fix: redundant Router alias to Navigation

--- lib/routes/home/widgets/user_bar.dart
+++ lib/routes/home/widgets/user_bar.dart
@@ -5,38 +5,37 @@ import "package:riba/utils/router.dart";
 import "package:riba/utils/theme.dart";
 
 class HomeUserBar extends StatelessWidget {
-  const HomeUserBar({super.key});
+	const HomeUserBar({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
+	@override
+	Widget build(BuildContext context) {
+		final theme = Theme.of(context);
+		final text = theme.textTheme;
 
-    return Container(
-      height: 64,
-      padding: Edges.horizontalSmall,
-      child: Row(children: [
-        IconButton(onPressed: () => {}, icon: const Icon(Icons.person)),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Guest", style: theme.textTheme.titleMedium?.copyWith(height: 0)),
-            Text(UserRole.member.asHumanReadable(),
-                style: theme.textTheme.bodySmall
-                    ?.withColorOpacity(0.5)
-                    .copyWith(letterSpacing: 0, height: 0))
-          ],
-        ),
-        const Spacer(),
-        PopupMenuButton<String>(
-          icon: const Icon(Icons.more_vert_rounded),
-          onSelected: (value) => Router.push(context, value),
-          offset: const Offset(-10, 56),
-          itemBuilder: (context) => [
-            const PopupMenuItem(value: Router.settings, child: Text("Settings")),
-          ],
-        ),
-      ]),
-    );
-  }
+		return Container(
+			height: 64,
+			padding: Edges.horizontalSmall,
+			child: Row(children: [
+				IconButton(onPressed: () => {}, icon: const Icon(Icons.person)),
+				Column(
+					mainAxisAlignment: MainAxisAlignment.center,
+					crossAxisAlignment: CrossAxisAlignment.start,
+					children: [
+						Text("Guest", style: text.titleMedium?.copyWith(height: 0)),
+						Text(UserRole.member.asHumanReadable(),
+							style: text.bodySmall?.withColorOpacity(0.5).copyWith(letterSpacing: 0, height: 0))
+					],
+				),
+				const Spacer(),
+				PopupMenuButton<String>(
+					icon: const Icon(Icons.more_vert_rounded),
+					onSelected: (value) => Navigator.pushNamed(context, value),
+					offset: const Offset(-10, 56),
+					itemBuilder: (context) => [
+						const PopupMenuItem(value: Router.settings, child: Text("Settings")),
+					],
+				),
+			]),
+		);
+	}
 }
