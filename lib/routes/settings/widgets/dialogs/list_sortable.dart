7cc7b5eaa9716ffc36e942254e21eeee6d65f99f
Rachala Ovin <resistlyfe@gmail.com>
2023-05-21 17:13:47 +0530

    chore: renamed variables to describe what they are

--- lib/routes/settings/widgets/dialogs/list_sortable.dart
+++ lib/routes/settings/widgets/dialogs/list_sortable.dart
@@ -27,8 +27,8 @@ class ListSortableDialog<T extends SerializableDataExt> extends StatefulWidget {
 }
 
 class _ListSortableDialogState<T extends SerializableDataExt> extends State<ListSortableDialog<T>> {
-	late final List<T> selectionMap = List.from(widget.currentValue);
-	late final List<T> idleMap = List.from(
+	late final List<T> selectionList = List.from(widget.currentValue);
+	late final List<T> idleList = List.from(
 		widget.values.where((e) => !widget.currentValue.contains(e)),
 	);
 
@@ -42,7 +42,7 @@ class _ListSortableDialogState<T extends SerializableDataExt> extends State<List
 			title: widget.title,
 			description: widget.description,
 			onReset: widget.onReset,
-			onConfirm: () => Navigator.pop(context, selectionMap),
+			onConfirm: () => Navigator.pop(context, selectionList),
 			sliver: buildSelectedList(text, colors),
 		);
 	}
@@ -50,22 +50,17 @@ class _ListSortableDialogState<T extends SerializableDataExt> extends State<List
 	Widget buildSelectedList(TextTheme text, ColorScheme colors) {
 		return SliverReorderableList(
 			// index starts with 0, so we don't need to account for the divider.
-			itemCount: selectionMap.length + idleMap.length,
+			itemCount: selectionList.length + idleList.length,
 			onReorder: handleReorder,
 			itemBuilder: (context, i) {
-				if (i == selectionMap.length) {
+				if (i == selectionList.length) {
 					return const Divider(key: ValueKey("divider"));
 				}
 
-				int actualIndex = i > selectionMap.length ? i - 1 : i;
-				T item;
-
-				if (i > selectionMap.length) {
-					item = idleMap.elementAt(actualIndex - selectionMap.length);
-				} else {
-					item = selectionMap.elementAt(i);
-				}
-
+				final actualIndex = i > selectionList.length ? i - 1 : i;
+				final T item = i > selectionList.length
+					? idleList.elementAt(actualIndex - selectionList.length)
+					: selectionList.elementAt(i);
 				final itemData = widget.itemBuilder(item);
 				
 				return Material(
@@ -90,29 +85,29 @@ class _ListSortableDialogState<T extends SerializableDataExt> extends State<List
 		// Accounts for the divider, so anything larger than selectionMap.length
 		// is in the idleMap. Subtract by selectionMap.length to get the index relative to
 		// the idleMap.
-		final oldActualIndex = oldIndex > selectionMap.length ? oldIndex - 1 : oldIndex;
-		final newActualIndex = newIndex > selectionMap.length ? newIndex - 1 : newIndex;
+		final oldActualIndex = oldIndex > selectionList.length ? oldIndex - 1 : oldIndex;
+		final newActualIndex = newIndex > selectionList.length ? newIndex - 1 : newIndex;
 
-		final wasInSelection = oldIndex < selectionMap.length;
-		final wasInIdle = oldIndex >= selectionMap.length;
+		final wasInSelection = oldIndex < selectionList.length;
+		final wasInIdle = oldIndex >= selectionList.length;
 		// Since these two depend on a list that could be grown by +1, 
 		// we will be using length instead of max index.
-		final isInSelection = newIndex <= selectionMap.length;
-		final isInIdle = newIndex > selectionMap.length;
+		final isInSelection = newIndex <= selectionList.length;
+		final isInIdle = newIndex > selectionList.length;
 
 		setState(() {
 			switch ((wasInSelection, wasInIdle, isInSelection, isInIdle)) {
 				case (true, false, true, false):
-					selectionMap.insert(newActualIndex, selectionMap.removeAt(oldIndex));
+					selectionList.insert(newActualIndex, selectionList.removeAt(oldIndex));
 					break;
 				case (true, false, false, true):
-					idleMap.insert(newActualIndex - selectionMap.length, selectionMap.removeAt(oldIndex));
+					idleList.insert(newActualIndex - selectionList.length, selectionList.removeAt(oldIndex));
 					break;
 				case (false, true, true, false):
-					selectionMap.insert(newActualIndex, idleMap.removeAt(oldActualIndex - selectionMap.length));
+					selectionList.insert(newActualIndex, idleList.removeAt(oldActualIndex - selectionList.length));
 					break;
 				case (false, true, false, true):
-					idleMap.insert(newActualIndex - selectionMap.length, idleMap.removeAt(oldActualIndex - selectionMap.length));
+					idleList.insert(newActualIndex - selectionList.length, idleList.removeAt(oldActualIndex - selectionList.length));
 					break;
 				default:
 					throw Exception("Invalid reorder combination");
